9f2dfd90f998bfbb6edd21fbab4f54a93b6ada24
MahdiAhazan <mambo.mehrabun@gmail.com>
2025-11-19 04:33:21 +0330

    Create theme_moove.php
    translated default en file

---
+++ lang/fa/theme_moove.php
@@ -0,0 +1,190 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_moove
+ * @copyright 2017 Willian Mano - http://conecti.me
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'موو (Moove)';
+$string['configtitle'] = 'موو';
+$string['choosereadme'] = 'موو یک پوسته مدرن و کاملاً قابل سفارشی‌سازی است. این پوسته برای استفاده مستقیم یا به‌عنوان پوسته والد هنگام ساخت پوسته‌های جدید با استفاده از بوت‌استرپ ۴ طراحی شده است.';
+
+$string['currentinparentheses'] = '(جاری)';
+$string['region-side-pre'] = 'راست';
+$string['prev_section'] = 'بخش قبلی';
+$string['next_section'] = 'بخش بعدی';
+$string['themedevelopedby'] = 'این پوسته با افتخار توسط';
+$string['themedevelopedbyconectimepartner'] = 'یک شرکت شریک مودل برزیلی توسعه داده شده است.';
+$string['needsupport'] = 'به پشتیبانی برای سایت مودل خود نیاز دارید؟ ';
+$string['pleasuretohelp'] = 'با کمال میل به شما کمک خواهیم کرد!';
+$string['access'] = 'ورود';
+$string['prev_activity'] = 'فعالیت قبلی';
+$string['next_activity'] = 'فعالیت بعدی';
+$string['donthaveanaccount'] = 'حساب کاربری ندارید؟';
+$string['signinwith'] = 'ورود با';
+
+// General settings tab.
+$string['generalsettings'] = 'عمومی';
+$string['logo'] = 'لوگو';
+$string['logodesc'] = 'لوگو در سربرگ سایت نمایش داده می‌شود.';
+$string['favicon'] = 'فاوآیکن سفارشی';
+$string['favicondesc'] = 'فاوآیکن دلخواه خود را بارگذاری کنید. باید فایل .ico باشد.';
+$string['preset'] = 'پیش‌تنظیم پوسته';
+$string['preset_desc'] = 'یک پیش‌تنظیم انتخاب کنید تا ظاهر کلی پوسته به‌طور گسترده تغییر کند.';
+$string['presetfiles'] = 'فایل‌های پیش‌تنظیم اضافی پوسته';
+$string['presetfiles_desc'] = 'از فایل‌های پیش‌تنظیم می‌توان برای تغییر چشمگیر ظاهر پوسته استفاده کرد. برای اطلاعات بیشتر در مورد ساخت و اشتراک فایل‌های پیش‌تنظیم به <a href="https://docs.moodle.org/dev/Boost_Presets">مستندات Boost presets</a> مراجعه کنید.';
+$string['loginbgimg'] = 'تصویر پس‌زمینه صفحه ورود';
+$string['loginbgimg_desc'] = 'تصویر پس‌زمینه دلخواه برای صفحه ورود بارگذاری کنید.';
+$string['brandcolor'] = 'رنگ برند';
+$string['brandcolor_desc'] = 'رنگ برجسته و اصلی سایت.';
+$string['secondarymenucolor'] = 'رنگ منوی ثانویه';
+$string['secondarymenucolor_desc'] = 'رنگ پس‌زمینه منوی ثانویه';
+$string['navbarbg'] = 'رنگ نوار ناوبری';
+$string['navbarbg_desc'] = 'رنگ نوار ناوبری سمت چپ';
+$string['navbarbghover'] = 'رنگ هاور نوار ناوبری';
+$string['navbarbghover_desc'] = 'رنگ نوار ناوبری سمت چپ هنگام هاور';
+$string['fontsite'] = 'قلم سایت';
+$string['fontsite_desc'] = 'قلم پیش‌فرض سایت. می‌توانید قلم‌ها را در <a href="https://fonts.google.com">سایت گوگل فونتس</a> امتحان کنید.';
+$string['enablecourseindex'] = 'فعال‌سازی فهرست درس';
+$string['enablecourseindex_desc'] = 'می‌توانید فهرست ناوبری درس را نمایش دهید یا مخفی کنید.';
+$string['enableclassicbreadcrumb'] = 'فعال‌سازی مسیر راهنما (breadcrumb) کلاسیک';
+$string['enableclassicbreadcrumb_desc'] = 'این تنظیم مسیر راهنمای کلاسیک را در صفحاتی مانند درس و دسته‌بندی‌ها نمایش می‌دهد.';
+
+// Advanced settings tab.
+$string['advancedsettings'] = 'پیشرفته';
+$string['rawscsspre'] = 'کد SCSS اولیه';
+$string['rawscsspre_desc'] = 'در این فیلد می‌توانید کد SCSS اولیه را وارد کنید که قبل از همه چیز تزریق می‌شود. معمولاً از این تنظیم برای تعریف متغیرها استفاده می‌شود.';
+$string['rawscss'] = 'کد SCSS اضافی';
+$string['rawscss_desc'] = 'از این فیلد برای افزودن کد SCSS یا CSS استفاده کنید که در انتهای فایل استایل تزریق می‌شود.';
+$string['googleanalytics'] = 'کد Google Analytics V4';
+$string['googleanalyticsdesc'] = 'کد Google Analytics V4 خود را وارد کنید (فرمت باید مانند G-XXXXXXXXXX باشد).';
+$string['hvpcss'] = 'CSS سفارشی H5P';
+$string['hvpcss_desc'] = 'از این فیلد برای افزودن CSS سفارشی که فقط در صفحات افزونه H5P تزریق می‌شود استفاده کنید.';
+
+// Frontpage settings tab.
+$string['frontpagesettings'] = 'صفحه اصلی';
+$string['displaymarketingboxes'] = 'نمایش جعبه‌های بازاریابی در صفحه اصلی';
+$string['displaymarketingboxesdesc'] = 'اگر می‌خواهید جعبه‌ها نمایش داده شوند، «بله» را انتخاب کنید و سپس <strong>ذخیره تغییرات</strong> را بزنید تا فیلدها بارگذاری شوند.';
+$string['marketingsectionheading'] = 'عنوان بخش بازاریابی';
+$string['marketingsectioncontent'] = 'محتوای بخش بازاریابی';
+$string['marketingicon'] = 'آیکن بازاریابی {$a}';
+$string['marketingheading'] = 'عنوان بازاریابی {$a}';
+$string['marketingcontent'] = 'محتوای بازاریابی {$a}';
+
+$string['disableteacherspic'] = 'مخفی کردن عکس مدرسان';
+$string['disableteacherspicdesc'] = 'این تنظیم عکس مدرسان را از کارت‌های درس مخفی می‌کند.';
+
+$string['sliderfrontpageloggedin'] = 'نمایش اسلایدر در صفحه اصلی پس از ورود؟';
+$string['sliderfrontpageloggedindesc'] = 'در صورت فعال بودن، اسلایدر در صفحه اصلی به جای تصویر سربرگ نمایش داده می‌شود.';
+$string['slidercount'] = 'تعداد اسلایدها';
+$string['slidercountdesc'] = 'تعداد اسلایدهایی که می‌خواهید اضافه کنید را انتخاب کنید و سپس <strong>ذخیره</strong> را بزنید تا فیلدها بارگذاری شوند.';
+$string['sliderimage'] = 'تصویر اسلاید';
+$string['sliderimagedesc'] = 'تصویری برای اسلاید اضافه کنید. اندازه پیشنهادی ۱۵۰۰×۵۴۰ پیکسل یا بزرگتر است.';
+$string['slidertitle'] = 'عنوان اسلاید';
+$string['slidertitledesc'] = 'عنوان اسلاید را اضافه کنید.';
+$string['slidercaption'] = 'توضیحات اسلاید';
+$string['slidercaptiondesc'] = 'توضیحات یا کپشن اسلاید را اضافه کنید.';
+
+$string['numbersfrontpage'] = 'نمایش آمار سایت در صفحه اصلی';
+$string['numbersfrontpagedesc'] = 'در صورت فعال بودن، تعداد کاربران فعال و درس‌ها در صفحه اصلی نمایش داده می‌شود.';
+$string['numbersfrontpagecontent'] = 'محتوای بخش آمار';
+$string['numbersfrontpagecontentdesc'] = 'می‌توانید هر متنی در سمت چپ بخش آمار اضافه کنید.';
+$string['numbersfrontpagecontentdefault'] = '<h2>مورد اعتماد بیش از ۲۵٬۰۰۰ مشتری خوشحال.</h2>
+                    <p>با تعداد زیادی بلوک منحصربه‌فرد، به‌راحتی و بدون کدنویسی می‌توانید<br class="d-none d-sm-block d-md-none d-xl-block">
+                        صفحه خود را بسازید. وب‌سایت بعدی خود را<br class="d-none d-sm-block d-md-none d-xl-block">
+                        در چند دقیقه بسازید.</p>';
+$string['numbersusers'] = 'کاربران فعالی که از منابع فوق‌العاده ما استفاده می‌کنند';
+$string['numberscourses'] = 'درس‌هایی که برای شما ساخته شده و می‌توانید به آن‌ها اعتماد کنید!';
+
+$string['faq'] = 'سوالات متداول';
+$string['faqcount'] = 'تعداد سوالات متداول';
+$string['faqcountdesc'] = 'تعداد سوالاتی که می‌خواهید اضافه کنید را انتخاب کنید و سپس <strong>ذخیره</strong> را بزنید.<br>اگر نمی‌خواهید بخش سوالات متداول داشته باشید، عدد ۰ را انتخاب کنید.';
+$string['faqquestion'] = 'سوال {$a}';
+$string['faqanswer'] = 'پاسخ {$a}';
+
+// Footer settings tab.
+$string['footersettings'] = 'پاورقی';
+$string['website'] = 'آدرس وب‌سایت';
+$string['websitedesc'] = 'وب‌سایت اصلی شرکت';
+$string['mobile'] = 'شماره موبایل';
+$string['mobiledesc'] = 'شماره موبایل را وارد کنید (مثال: +989123456789)';
+$string['mail'] = 'ایمیل';
+$string['maildesc'] = 'ایمیل پشتیبانی شرکت';
+$string['tiktok'] = 'آدرس تیک‌تاک';
+$string['tiktokdesc'] = 'آدرس تیک‌تاک خود را وارد کنید (مثال: https://www.tiktok.com/@myinstitution)';
+$string['facebook'] = 'آدرس فیس‌بوک';
+$string['facebookdesc'] = 'آدرس صفحه فیس‌بوک را وارد کنید (مثال: https://www.facebook.com/myinstitution)';
+$string['twitter'] = 'آدرس توییتر (X)';
+$string['twitterdesc'] = 'آدرس توییتر خود را وارد کنید (مثال: https://twitter.com/myinstitution)';
+$string['linkedin'] = 'آدرس لینکدین';
+$string['linkedindesc'] = 'آدرس لینکدین خود را وارد کنید (مثال: https://www.linkedin.com/company/myinstitution)';
+$string['youtube'] = 'آدرس یوتیوب';
+$string['youtubedesc'] = 'آدرس کانال یوتیوب را وارد کنید (مثال: https://www.youtube.com/c/myinstitution)';
+$string['instagram'] = 'آدرس اینستاگرام';
+$string['instagramdesc'] = 'آدرس اینستاگرام خود را وارد کنید (مثال: https://www.instagram.com/myinstitution)';
+$string['pinterest'] = 'آدرس پینترست';
+$string['pinterestdesc'] = 'آدرس پینترست خود را وارد کنید (مثال: https://www.pinterest.com/myinstitution)';
+$string['whatsapp'] = 'شماره واتس‌اپ';
+$string['whatsappdesc'] = 'شماره واتس‌اپ برای تماس را وارد کنید.';
+$string['telegram'] = 'تلگرام';
+$string['telegramdesc'] = 'لینک کانال یا گروه تلگرام را وارد کنید.';
+$string['contactus'] = 'تماس با ما';
+$string['followus'] = 'ما را دنبال کنید';
+
+// Mypublic page.
+$string['aboutme'] = 'درباره من';
+$string['personalinformation'] = 'اطلاعات شخصی';
+$string['addcontact'] = 'افزودن تماس';
+$string['removecontact'] = 'حذف تماس';
+
+// Theme settings.
+$string['themesettings:accessibility'] = 'دسترس‌پذیری';
+$string['themesettings:fonttype'] = 'نوع قلم';
+$string['themesettings:defaultfont'] = 'قلم پیش‌فرض';
+$string['themesettings:dyslexicfont'] = 'قلم ویژه دیسلکسیا';
+$string['themesettings:enableaccessibilitytoolbar'] = 'فعال‌سازی نوار ابزار دسترس‌پذیری';
+$string['themesettingg:successfullysaved'] = 'تنظیمات دسترس‌پذیری با موفقیت ذخیره شد';
+
+// Accessibility features.
+$string['accessibility:fontsize'] = 'اندازه قلم';
+$string['accessibility:decreasefont'] = 'کاهش اندازه قلم';
+$string['accessibility:resetfont'] = 'بازنشانی اندازه قلم';
+$string['accessibility:increasefont'] = 'افزایش اندازه قلم';
+$string['accessibility:sitecolor'] = 'رنگ‌بندی سایت';
+$string['accessibility:resetsitecolor'] = 'بازنشانی رنگ‌بندی سایت';
+$string['accessibility:sitecolor2'] = 'کنتراست پایین ۱';
+$string['accessibility:sitecolor3'] = 'کنتراست پایین ۲';
+$string['accessibility:sitecolor4'] = 'کنتراست بالا';
+
+// Data privacy.
+$string['privacy:metadata:preference:accessibilitystyles_fontsizeclass'] = 'ترجیح کاربر برای اندازه قلم.';
+$string['privacy:metadata:preference:accessibilitystyles_sitecolorclass'] = 'ترجیح کاربر برای رنگ‌بندی سایت.';
+$string['privacy:metadata:preference:thememoovesettings_fonttype'] = 'ترجیح کاربر برای نوع قلم.';
+$string['privacy:metadata:preference:thememoovesettings_enableaccessibilitytoolbar'] = 'ترجیح کاربر برای نمایش نوار ابزار دسترس‌پذیری.';
+
+$string['privacy:accessibilitystyles_fontsizeclass'] = 'ترجیح فعلی برای اندازه قلم: {$a}.';
+$string['privacy:accessibilitystyles_sitecolorclass'] = 'ترجیح فعلی برای رنگ‌بندی سایت: {$a}.';
+$string['privacy:thememoovesettings_fonttype'] = 'ترجیح فعلی برای نوع قلم: {$a}.';
+$string['privacy:thememoovesettings_enableaccessibilitytoolbar'] = 'ترجیح فعلی برای نمایش نوار ابزار دسترس‌پذیری فعال است.';
+
+$string['redirectmessage'] = 'این صفحه باید به‌صورت خودکار هدایت شود.';
+$string['redirectbtntext'] = 'اگر اتفاقی نیفتاد، لطفاً اینجا کلیک کنید تا ادامه دهید.';
